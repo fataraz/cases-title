3aed97ac7dd924b568ccf1a1bc18e0a007ce3646
Razif Fatahillah Kahfi <fatahillah.razif@gmail.com>
2023-09-22 15:52:22 +0700

    fixing

--- code/message.go
+++ code/message.go
@@ -13,6 +13,8 @@ func ConvertWord(words string) (result string) {
 	for _, word := range wordsArr {
 		if cases.Lower(language.English).String(word) == "of" || cases.Lower(language.English).String(word) == "dan" {
 			result += cases.Lower(language.English).String(word) + " "
+		} else if cases.Lower(language.English).String(word) == "php," || cases.Lower(language.English).String(word) == "go," || cases.Lower(language.English).String(word) == "CSS," {
+			result += strings.ToUpper(word) + " "
 		} else {
 			result += cases.Title(language.English).String(word) + " "
 		}
