6fd4803f9bb44bc59ca140c6148074f4098381d6
Razif Fatahillah Kahfi <fatahillah.razif@gmail.com>
2023-09-22 20:27:38 +0700

    refactor

--- code/message.go
+++ code/message.go
@@ -9,13 +9,27 @@ import (
 
 // ConvertWord ....
 func ConvertWord(words string) (result string) {
-	wordsArr := strings.Split(words, " ")
+	// split with \n
+	wordsSplit := strings.Split(words, "\n")
+	// array to string
+	wordsStr := strings.Join(wordsSplit, " ")
+	// split with space
+	wordsArr := strings.Split(wordsStr, " ")
+
 	for _, word := range wordsArr {
-		if cases.Lower(language.English).String(word) == "of" || cases.Lower(language.English).String(word) == "dan" {
+		switch cases.Lower(language.English).String(word) {
+		case "of", "dan":
 			result += cases.Lower(language.English).String(word) + " "
-		} else if cases.Lower(language.English).String(word) == "php," || cases.Lower(language.English).String(word) == "go," || cases.Lower(language.English).String(word) == "css," {
+		case "php,", "go,", "css,":
 			result += strings.ToUpper(word) + " "
-		} else {
+		case "interview", "15:22:32":
+			result += cases.Title(language.English).String(word) + "\n\n "
+		case "sekolah", "tanggal", "terima":
+			result += "\n  " + cases.Title(language.English).String(word) + " "
+		case "nama:", "jam:", "languages:":
+			char := word[:len(word)-1] // remove last character in string
+			result += cases.Title(language.English).String(char) + " : "
+		default:
 			result += cases.Title(language.English).String(word) + " "
 		}
 	}
